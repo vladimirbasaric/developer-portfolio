2634c59c16a3272326c3804baaff728a511062d6
vladimirbasaric <vladimirbasaric@gmail.com>
2020-08-15 17:43:40 +0200

    aos added

--- assets/js/main.js
+++ assets/js/main.js
@@ -153,4 +153,15 @@
     });
   });
 
+  // Init AOS
+  function aos_init() {
+    AOS.init({
+      duration: 1000,
+      once: true
+    });
+  }
+  $(window).on('load', function() {
+    aos_init();
+  });
+
 })(jQuery);
